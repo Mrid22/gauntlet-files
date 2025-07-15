5f16cc2053f6ed17b783b7190bea9d9eaacb09ad
Mrid22 <mridulaga@outlook.com>
2025-07-15 18:28:34 +0800

    setup

--- src/template-view.tsx
+++ src/template-view.tsx
@@ -1,19 +1,16 @@
-import { ReactElement } from 'react';
+import { ReactElement } from "react";
 import { Detail } from "@project-gauntlet/api/components";
 import { Environment } from "@project-gauntlet/api/helpers";
 
 export default function TemplateView(): ReactElement {
-    return (
-        <Detail>
-            <Detail.Content>
-                <Detail.Content.Paragraph>
-                    Hello Gauntlet!
-                </Detail.Content.Paragraph>
-                <Detail.Content.Paragraph>
-                    Gauntlet Version: {Environment.gauntletVersion}
-                </Detail.Content.Paragraph>
-            </Detail.Content>
-        </Detail>
-    );
-};
-
+  return (
+    <Detail>
+      <Detail.Content>
+        <Detail.Content.Paragraph>Hello Gauntlet!</Detail.Content.Paragraph>
+        <Detail.Content.Paragraph>
+          Gauntlet Version: {Environment.gauntletVersion}
+        </Detail.Content.Paragraph>
+      </Detail.Content>
+    </Detail>
+  );
+}
